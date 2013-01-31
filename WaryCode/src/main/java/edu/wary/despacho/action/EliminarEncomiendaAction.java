5c491cca4a67431bc48dd3d17f95cc0ddb1b2253
miguel87galarza@gmail.com <miguel87galarza@gmail.com@f162d8a2-956b-0cdb-ff30-987d44b5fce8>
2013-01-31 20:41:01 +0000

    Se modifico la clase Action Eliminar para el Java docs

--- WaryCode/src/main/java/edu/wary/despacho/action/EliminarEncomiendaAction.java
+++ WaryCode/src/main/java/edu/wary/despacho/action/EliminarEncomiendaAction.java
@@ -3,12 +3,25 @@ package edu.wary.despacho.action;
 import edu.wary.despacho.service.DespachoService;
 import edu.wary.model.Encomienda;
 
+/**
+ * Metodo que permite ELIMINAR LAS ENCOMIENDAS
+ * @author galatea-summer-programador-MGalarza
+ * @version 1.0
+ * @since 25-01-2013
+ */
 public class EliminarEncomiendaAction {
 	private int idEnco;
 	private String mensajeEE;
 	private DespachoService service=new DespachoService();
 	
+	/**
+	 * Define el metodo que eliminara la encomienda
+	 */
+
 	public String execute(){
+		/**
+		 * Define el camino que llevara el mensaje y el jsp de respuesta
+		 */
 		String camino="success";
 		Encomienda encomienda=new Encomienda();
 		encomienda.setIdEnco(idEnco);
--- WaryCode/src/main/java/edu/wary/despacho/action/EliminarIncidenciaAction.java
+++ WaryCode/src/main/java/edu/wary/despacho/action/EliminarIncidenciaAction.java
@@ -3,18 +3,32 @@ package edu.wary.despacho.action;
 import edu.wary.despacho.service.DespachoService;
 import edu.wary.model.Incidencia;
 
+/**
+ * Metodo que permite ELIMINAR LAS INCIDENCIAS
+ * @author galatea-summer-programador-MGalarza
+ * @version 1.0
+ * @since 25-01-2013
+ */
+
 public class EliminarIncidenciaAction {
 	private int idInc;
 	private String mensajeEE;
 	private DespachoService service=new DespachoService();
 	
+	/**
+	 * Define el metodo que eliminara la una incidencia
+	 */
+	
 	public String execute(){
+		/**
+		 * Define el camino que llevara el mensaje y el jsp de respuesta
+		 */
 		String camino="success";
 		Incidencia incidencia=new Incidencia();
 		incidencia.setIdInc(idInc);
 		try {
 			service.eliminarInc(incidencia);
-			mensajeEE="entrega satisfactoria";
+			mensajeEE="satisfactoria";
 		} catch (Exception e) {
 			// TODO: handle exception
 			mensajeEE=e.getMessage();
