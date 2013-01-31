46cb45ddaf83e1de56d2891a4db47a7fc9ba0e5b
miguel87galarza@gmail.com <miguel87galarza@gmail.com@f162d8a2-956b-0cdb-ff30-987d44b5fce8>
2013-01-31 22:41:55 +0000

    Nueva clase ListarIncidenciaAction

---
+++ WaryCode/src/main/java/edu/wary/despacho/action/ListarIncidenciasAction.java
@@ -0,0 +1,41 @@
+package edu.wary.despacho.action;
+
+import java.util.List;
+
+import edu.wary.despacho.service.DespachoService;
+import edu.wary.model.Incidencia;
+
+
+/**
+ * Metodo que permite LISTAR LAS INCIDENCIAS
+ * @author galatea-summer-programador-MGalarza
+ * @version 1.0
+ * @since 25-01-2013
+ */
+
+public class ListarIncidenciasAction {
+	private List<Incidencia> incidencias;
+
+	/**
+	 * Define el metodo que eliminara la Incidencia
+	 */
+	
+	public String execute(){
+		/**
+		 * Define el camino que llevara el mensaje y el jsp de respuesta
+		 */
+		String camino="success";
+		DespachoService service=new DespachoService();
+		incidencias=service.listarIncidencia();
+		return camino;
+	}
+
+	public List<Incidencia> getIncidencias() {
+		return incidencias;
+	}
+
+	public void setIncidencias(List<Incidencia> incidencias) {
+		this.incidencias = incidencias;
+	}
+	
+}
