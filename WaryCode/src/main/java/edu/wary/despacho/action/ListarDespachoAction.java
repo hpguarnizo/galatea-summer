cea53a73880510a0517cba0816941a302abc9829
miguel87galarza@gmail.com <miguel87galarza@gmail.com@f162d8a2-956b-0cdb-ff30-987d44b5fce8>
2013-01-31 20:42:45 +0000

    Se modifico las clases listar Action para javaDocs

--- WaryCode/src/main/java/edu/wary/despacho/action/ListarDespachoAction.java
+++ WaryCode/src/main/java/edu/wary/despacho/action/ListarDespachoAction.java
@@ -5,10 +5,24 @@ import java.util.List;
 import edu.wary.despacho.service.DespachoService;
 import edu.wary.model.Encomienda;
 
+/**
+ * Metodo que permite LISTAR LAS ENCOMIENDAS DEL DESPACHO
+ * @author galatea-summer-programador-MGalarza
+ * @version 1.0
+ * @since 25-01-2013
+ */
+
 public class ListarDespachoAction {
 	private List<Encomienda> despachos;
 	
+	/**
+	 * Define el metodo que lista las encomiendas en el despacho
+	 */
+	
 	public String execute(){
+		/**
+		 * Define el camino que llevara el mensaje y el jsp de respuesta
+		 */
 		String camino="success";
 		DespachoService service=new DespachoService();
 		despachos=service.listarDespachos();
--- WaryCode/src/main/java/edu/wary/despacho/action/ListarEncomiendasAction.java
+++ WaryCode/src/main/java/edu/wary/despacho/action/ListarEncomiendasAction.java
@@ -5,10 +5,25 @@ import java.util.List;
 import edu.wary.despacho.service.DespachoService;
 import edu.wary.model.Encomienda;
 
+
+/**
+ * Metodo que permite LISTAR LAS ENCOMIENDAS
+ * @author galatea-summer-programador-MGalarza
+ * @version 1.0
+ * @since 25-01-2013
+ */
+
 public class ListarEncomiendasAction {
 	private List<Encomienda> encomiendas;
+
+	/**
+	 * Define el metodo que eliminara la encomienda
+	 */
 	
 	public String execute(){
+		/**
+		 * Define el camino que llevara el mensaje y el jsp de respuesta
+		 */
 		String camino="success";
 		DespachoService service=new DespachoService();
 		encomiendas=service.listarEncomienda();
