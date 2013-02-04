3d901c349e2cf2edad7a880a5c9bf60c7ba66737
hugadica.net@gmail.com <hugadica.net@gmail.com@f162d8a2-956b-0cdb-ff30-987d44b5fce8>
2013-02-04 19:03:40 +0000

    agregar nuevo EncomiendaService

---
+++ WaryCode/src/main/java/edu/wary/despacho/service/EncomiendaService.java
@@ -0,0 +1,44 @@
+package edu.wary.despacho.service;
+import java.util.List;
+
+import javax.management.RuntimeErrorException;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+
+import edu.wary.model.Almacen;
+import edu.wary.model.Destinatario;
+import edu.wary.model.Encomienda;
+import edu.wary.model.Cliente;
+import edu.wary.model.Incidencia;
+import edu.wary.model.Pago;
+import edu.wary.model.Remitente;
+import edu.wary.model.repository.AlmacenRepository;
+import edu.wary.model.repository.DestinatarioRepository;
+import edu.wary.model.repository.EncomiendaRepository;
+
+import edu.wary.model.repository.ClienteRepository;
+import edu.wary.model.repository.IncidenciaRepository;
+import edu.wary.model.repository.PagoRepository;
+import edu.wary.model.repository.RemitenteRepository;
+import edu.wary.util.Utilitario;
+
+@SuppressWarnings("unused")
+public class EncomiendaService {
+
+	private EncomiendaRepository encomiendaRepository;
+	private EntityManager em;
+	public  EncomiendaService() {
+		
+		em=Utilitario.getInstance().getEntityManager();
+		
+	}
+	public Cliente buscarCliente(Cliente e)throws RuntimeException{
+		ClienteRepository clienteRepository=new ClienteRepository(em);
+		Cliente clientebd=clienteRepository.findById(e.getIdCod());
+		if (clientebd.getIdCod()!=e.getIdCod()) {
+			throw new RuntimeException("Cliente no encontrado");
+		}
+		return clientebd;
+	}
+}
